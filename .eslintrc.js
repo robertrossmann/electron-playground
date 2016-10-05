42c545f1ea3b9e814181ac182cc2a22d2a903939
Robert Rossmann <rr.rossmann@me.com>
2016-10-05 16:47:18 +0200

    Switch to js-coding-standards 6.x

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,27 +4,19 @@ module.exports = {
   parser: 'babel-eslint',
 
   plugins: [
-    'babel'
+    'babel',
+    'import'
   ],
 
   extends: [
     '@strv/javascript/environments/nodejs/v6',
     '@strv/javascript/environments/nodejs/best-practices',
     '@strv/javascript/environments/nodejs/optional',
-    '@strv/javascript/coding-styles/base'
+    '@strv/javascript/coding-styles/base',
   ],
 
   rules: {
     // If your editor cannot show these to you, occasionally turn this off and run the linter
-    'no-warning-comments': 0,
-
-    // Fix false positives for some ESLint rules - they do not properly handle async functions
-    'arrow-parens': 0,
-    'babel/arrow-parens': [1, 'as-needed'],
-    'generator-star-spacing': 0,
-    'babel/generator-star-spacing': [1, {
-      before: false,
-      after: true
-    }]
+    'no-warning-comments': 0
   }
 }
