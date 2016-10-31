44a05a3837617ffd69b83bbfc643ff551caf67c6
Robert Rossmann <rr.rossmann@me.com>
2016-10-31 17:14:56 +0100

    Much work on Event detail 🚀

--- src/renderer/components/event-manager/event-detail/event-detail.jsx
+++ src/renderer/components/event-manager/event-detail/event-detail.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import {
+  Divider,
+  Table,
+  TableBody,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui'
 import { connect } from 'react-redux'
 
 function EventDetail(props) {
@@ -10,8 +17,30 @@ function EventDetail(props) {
 
   return (
     <div id="event-detail">
-      <h2>{event.title}</h2>
       <img className="cover-art" src={event.coverUrl} />
+      <h3>{event.title}</h3>
+      <Divider />
+      <Section title="General information">
+        <Row title="Kind" value={event.kind} />
+        <Row title="Current state" value={event.state} />
+        <Row title="Starts at" value={event.startsAt} />
+        <Row title="Finished at" value={event.finishedAt || '---'} />
+      </Section>
+      <Section title="Pricing & availability" last>
+        <Row title="Available on" value={event.purchasesOpenAt || '---'} />
+        <Row
+          title="Purchase time"
+          value={
+            event.purchaseWindow
+              ? `${event.purchaseWindow} min`
+              : '---'
+          }
+        />
+        <Row title="Presence required" value={event.requiresPresence ? 'Yes' : 'No'} />
+        <Row title="Track purchases allowed" value={event.allowsTrackPurchases ? 'Yes' : 'No'} />
+        <Row title="Event price" value={`$${event.eventSKU.split('.').pop() - 0.01}`} />
+        <Row title="Track price" value={`$${event.trackSKU.split('.').pop() - 0.01}`} />
+      </Section>
     </div>
   )
 }
@@ -19,6 +48,43 @@ function EventDetail(props) {
 EventDetail.propTypes = {
   event: React.PropTypes.object,
 }
+
+
+function Section(props) {
+  return (
+    <div>
+      <h4>{props.title}</h4>
+      <Table selectable={false}>
+        <TableBody displayRowCheckbox={false}>
+          {props.children}
+        </TableBody>
+      </Table>
+      {!props.last && <Divider />}
+    </div>
+  )
+}
+
+Section.propTypes = {
+  children: React.PropTypes.node,
+  last: React.PropTypes.bool,
+  title: React.PropTypes.string.isRequired,
+}
+
+
+function Row(props) {
+  return (
+    <TableRow displayBorder={false}>
+      <TableRowColumn>{props.title}</TableRowColumn>
+      <TableRowColumn>{props.value}</TableRowColumn>
+    </TableRow>
+  )
+}
+
+Row.propTypes = {
+  title: React.PropTypes.string.isRequired,
+  value: React.PropTypes.string.isRequired,
+}
+
 
 /**
  * Map state to component props
@@ -29,11 +95,26 @@ EventDetail.propTypes = {
  */
 function mapState(state) {
   const current = state.events.getIn(['meta', 'current'])
+  const event = current
+    ? state.events.getIn(['entries', current]).toJS()
+    : null
+  const dates = [
+    'startsAt',
+    'finishedAt',
+    'purchasesOpenAt',
+  ]
+
+  if (event) {
+    // Normalise some fields for display
+    for (const field of dates) {
+      if (event[field]) {
+        event[field] = new Date(event[field]).toLocaleString()
+      }
+    }
+  }
 
   return {
-    event: current
-      ? state.events.getIn(['entries', current]).toJS()
-      : null,
+    event,
   }
 }
 
